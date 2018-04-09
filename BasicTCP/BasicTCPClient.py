9edfb0c4aa16cf24babf86b504d50e64e24a128c
Unknown <gunhoo2016@naver.com>
2018-04-09 17:17:29 +0900

    Basic TCP socket programming
    By using TCP, server and client are communicate each other.
    function1 is change message to upper letter.
    function2 is change message to lower letter.
    function3 is get my IP and port number.
    function4 is get server time.

---
+++ BasicTCP/BasicTCPClient.py
@@ -0,0 +1,79 @@
+# BasicTCPClient.py
+#
+
+from socket import *
+import time
+
+serverName = #server name
+serverPort = #server port
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+print("The client is running on port", clientSocket.getsockname()[1])
+
+while True :
+    try:
+        print('')
+        print('<Menu>')
+        print('1) convert text to UPPER-case')
+        print('2) convert text to LOWER-case')
+        print('3) get my IP address and port number')
+        print('4) get server time')
+        print('5) exit')
+        option = input('Input option: ')
+        if option == '1':
+            message = input('Input lowercase sentence: ')
+            print('')
+            modifiedMessage = option + 'message' + message
+            start_time = time.time()
+            clientSocket.send(modifiedMessage.encode())
+            receivedMessage = clientSocket.recv(2048)
+            end_time = time.time()
+            print('Reply from server:', receivedMessage.decode())
+            print('Response time : ', (end_time - start_time)*1000, 'ms')
+            continue
+        elif option == '2':
+            message = input('Input uppercase sentence: ')
+            print('')
+            modifiedMessage = option + 'message' + message
+            start_time = time.time()
+            clientSocket.send(modifiedMessage.encode())
+            receivedMessage = clientSocket.recv(2048)
+            end_time = time.time()
+            print('Relpy from server:', receivedMessage.decode())
+            print('Response time : ', (end_time - start_time)*1000, 'ms')
+            continue
+        elif option == '3':
+            print('')
+            start_time = time.time()
+            clientSocket.send(option.encode())
+            ipMessage = clientSocket.recv(2048)
+            end_time = time.time()
+            modifiedMessage = ipMessage.decode().split('port')
+            print('Reply from server: IP =', modifiedMessage[0], 'port=', modifiedMessage[1])
+            print('Response time: ', (end_time - start_time)*1000, 'ms' )
+            continue
+        elif option == '4':
+            print('')
+            start_time = time.time()
+            clientSocket.send(option.encode())
+            modifiedMessage = clientSocket.recv(2048)
+            end_time = time.time()
+            print('Reply from server: time =', modifiedMessage.decode())
+            print('Response time: ', (end_time - start_time)*1000, 'ms')
+            continue
+        elif option == '5':
+            clientSocket.send(option.encode())
+            print('Bye bye~')
+            clientSocket.close()
+            break
+        else :
+            print('Input error')
+            continue
+    except KeyboardInterrupt:
+        print('')
+        print('Bye bye~')
+        clientSocket.send(str(5).encode())
+        clientSocket.close()
+        exit()
